0fa437056ba89a31832236390ccfd56bdb93657d
eleung32 <52718704+eleung32@users.noreply.github.com>
2020-12-29 16:56:59 -0500

    building components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-//import logo from './logo.svg';
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      {/*<Logo />
-      <ImageLinkForm />
-      <FaceRecognition />*/}
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        {/*
+        <ImageLinkForm />
+        <FaceRecognition />*/}
+      </div>
+    );
+  }      
 }
 
 export default App;
