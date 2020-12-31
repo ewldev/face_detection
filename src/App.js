66d460515d652d541497df562b185563230da09c
eleung32 <52718704+eleung32@users.noreply.github.com>
2020-12-30 22:30:56 -0500

    installed clarifai, modified app.js and FaceRecognition.js, tested Clarifai.COLOR_MODEL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'af8ce459695542f5b8c97e528864eee1'
+ });
 
 const particlesOptions = {
   particles: {
@@ -28,10 +34,57 @@ class App extends Component {
   onInputChange =(event) => {
     console.log (event.target.value);
   }
-  onButtonSubmit = () => {
-    console.log('click');
-  }
+  //onButtonSubmit = () => {
+  //   this.setState({imageUrl: this.state.input});
+  //   app.models
+  //     .predict(
+  //       // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+  //       // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+  //       // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+  //       // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+  //       // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
+  //       // so you would change from:
+  //       // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+  //       // to:
+  //       // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+  //       Clarifai.FACE_DETECT_MODEL,
+  //       this.state.input)
+  //     .then(response => {
+  //       console.log('hi', response)
+  //       if (response) {
+  //         fetch('http://localhost:3000/image', {
+  //           method: 'put',
+  //           headers: {'Content-Type': 'application/json'},
+  //           body: JSON.stringify({
+  //             id: this.state.user.id
+  //           })
+  //         })
+  //           .then(response => response.json())
+  //           .then(count => {
+  //             this.setState(Object.assign(this.state.user, { entries: count}))
+  //           })
 
+  //       }
+  //       this.displayFaceBox(this.calculateFaceLocation(response))
+  //     })
+  //     .catch(err => console.log(err));
+  // 
+
+  //
+  onButtonSubmit = () => { 
+  console.log ('click');
+  app.models
+      .predict(
+        Clarifai.COLOR_MODEL,
+        "https://samples.clarifai.com/face-det.jpg")
+      .then(
+      function (response){
+        console.log(response);
+      },
+      function(err){
+      }
+    );
+  }
   render(){
     return (
       <div className="App">
@@ -45,8 +98,7 @@ class App extends Component {
            onInputChange={this.onInputChange}
            onButtonSubmit={this.onButtonSubmit}
         />
-         {/*
-        <FaceRecognition />*/}
+        <FaceRecognition />
       </div>
     );
   }      
