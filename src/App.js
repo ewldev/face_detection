44339bfaf391301e019679f818a00e5ef954da2f
eleung32 <52718704+eleung32@users.noreply.github.com>
2020-12-31 12:04:30 -0500

    updated app.js FaceRecognition.js and logo.js for face detection box

--- src/App.js
+++ src/App.js
@@ -29,28 +29,32 @@ class App extends Component {
     super();
       this.state ={
         input: '',
-        imageUrl:''
+        imageUrl: '',
+        box: {},
       }
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+  }  
   onInputChange =(event) => {
     this.setState ({input: event.target.value});
   }
     
   onButtonSubmit = () => { 
-  this.setState({imageUrl: this.state.input});
-     app.models
-       .predict(
-         Clarifai.FACE_DETECT_MODEL,
-         this.state.input)
-        .then(
-        function (response){
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-        function(err){
-       }
-     );
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => this.calculateFaceLocation(response))
+      .catch(err => console.log(err));    
   }
-
+   
   render(){
     return (
       <div className="App">
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import './Logo.css';
 
 const Logo = () => {
     return (
-        <div className='ma4 mt0'>
+        <div className='center ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner pa3 ">
                     <img style={{paddingTop: '6px'}} alt='logo' src={ai}/>
