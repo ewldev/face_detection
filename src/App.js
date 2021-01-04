2ec9c1e7711fef3ffba90f06a30baef3df741899
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-01-04 12:36:23 -0500

    implemented face detection box

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
@@ -39,8 +40,19 @@ class App extends Component {
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log(width, height);
-  }  
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+  
+  displayFaceBox = (box) => {
+    //console.log(box);
+    this.setState({box: box});
+  }
+
   onInputChange =(event) => {
     this.setState ({input: event.target.value});
   }
@@ -51,7 +63,7 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(response => this.calculateFaceLocation(response))
+      .then(response => this.displayFaceBox (this.calculateFaceLocation(response)))
       .catch(err => console.log(err));    
   }
    
@@ -62,13 +74,14 @@ class App extends Component {
       params={particlesOptions}
     />
         <Navigation />
+        <Signin />
         <Logo />  
         <Rank />
         <ImageLinkForm 
            onInputChange={this.onInputChange}
            onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }      
